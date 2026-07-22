1a7b3a14cfa0beab1f47d50f45eaff7a1ebb354d
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-07-21 22:32:29 -0300

    Configura pra usar bando de dados no servidor

--- db/conn.go
+++ db/conn.go
@@ -9,16 +9,16 @@ import (
 
 const (
 	// Database connection parameters
-	host     = "go_db"
+	host     = "dpg-d9g0rrnlk1mc73c4u82g-a.ohio-postgres.render.com"
 	port     = 5432
-	user     = "postgres"
-	password = "1234"
-	dbname   = "postgres"
+	user     = "joel"
+	password = "zCY8YOMEdXwaePaVpH88kPVsR4I78FOI"
+	dbname   = "curriculum_ch6o"
 )
 
 func ConnectDB() (*sql.DB, error) {
 	// Create a connection string
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=verify-full",
 		host, port, user, password, dbname)
 
 	// Open a connection to the database
