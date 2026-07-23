cc0c7d4f6a48088df3513ef6e1aebfbfc9ae0c7b
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-07-22 22:41:05 -0300

    Aponta para banco de dados supabase

--- db/conn.go
+++ db/conn.go
@@ -9,16 +9,16 @@ import (
 
 const (
 	// Database connection parameters
-	host     = "dpg-d9g0rrnlk1mc73c4u82g-a.ohio-postgres.render.com"
-	port     = 5432
-	user     = "joel"
-	password = "zCY8YOMEdXwaePaVpH88kPVsR4I78FOI"
-	dbname   = "curriculum_ch6o"
+	host     = "aws-0-sa-east-1.pooler.supabase.com"
+	port     = 6543
+	user     = "postgres.fmdbezyxzpozkupibykm"
+	password = "r5i9BxqNxRKtP8Ym"
+	dbname   = "postgres"
 )
 
 func ConnectDB() (*sql.DB, error) {
 	// Create a connection string
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=verify-full",
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
 	// Open a connection to the database
