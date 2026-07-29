f3d48092a8ff80dfbef9f6f01184a9259742bacd
Joel de Almeida Souza <joelsouza82@gmail.com>
2026-07-28 22:25:56 -0300

    remove product

--- cmd/main.go
+++ cmd/main.go
@@ -16,22 +16,14 @@ func main() {
 		panic("Erro de conexão com o banco de dados: " + err.Error())
 	}
 
-	ProductRepository := repository.NewProductRepository(dbConnection)
-	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
-	ProductController := controller.NewProductController(ProductUseCase)
-
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	server.GET("/products", ProductController.GetProducts)
-	server.POST("/product", ProductController.CreateProduct)
-	server.GET("/product/:productId", ProductController.GetProductById)
-
 	PersonalRepository := repository.NewPersonalRepository(dbConnection)
-	PersonalUseCase := usecase.NewPersonalUseCase(PersonalRepository) // Nenhuma mudança necessária aqui, Go lida com isso.
+	PersonalUseCase := usecase.NewPersonalUseCase(PersonalRepository)
 	PersonalController := controller.NewPersonalController(PersonalUseCase)
 
 	server.GET("/personals", PersonalController.GetPersonals)
